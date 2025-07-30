8b31160b44d6dcf3fe9734624f9faeca24c953ed
Henrique Erculano <henriqueerculano@gmail.com>
2025-07-30 10:47:05 -0300

    atualizando o código

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-let nome = "Felipe";
-console.log(nome);
+// Dados do herói
+let nome = "Arthas";
+let xp = 2280;
+let nivel = "";
+
+// Usando laço de repetição apenas 1 vez, conforme solicitado
+for (let i = 0; i < 1; i++) {
+  if (xp < 1000) {
+    nivel = "Ferro";
+  } else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+  } else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+  } else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+  } else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+  } else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+  } else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+  } else if (xp >= 10001) {
+    nivel = "Radiante";
+  }
+
+  // Mensagem final
+  console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
