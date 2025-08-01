2e79061d24825f73c39071abf84e127f6f14ca2b
riqueerculano <henriqueerculano@gmail.com>
2025-08-01 11:51:59 -0300

    código finalizado

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 // Dados do herói
-let nome = "Arthas";
-let xp = 2280;
-let nivel = "";
+let herois = [
+{ nome: "Arthurus", xp: 2280 },
+{ nome: "Sylvana", xp: 980 },
+{ nome: "Uriel", xp: 7050 },
+{ nome: "Galadriel", xp: 10500 },
+{ nome: "Thanos", xp: 8500 }
+]
 
-// Usando laço de repetição apenas 1 vez, conforme solicitado
-for (let i = 0; i < 1; i++) {
-  if (xp < 1000) {
+// Laço de repetição para processar todos os heróis
+for (let i = 0; i < herois.length; i++) {
+  let heroi = herois[i];
+  let nivel = "";
+
+  // Estrutura de decisão para definir o nível
+  if (heroi.xp < 1000) {
     nivel = "Ferro";
-  } else if (xp >= 1001 && xp <= 2000) {
+  } else if (heroi.xp >= 1001 && heroi.xp <= 2000) {
     nivel = "Bronze";
-  } else if (xp >= 2001 && xp <= 5000) {
+  } else if (heroi.xp >= 2001 && heroi.xp <= 5000) {
     nivel = "Prata";
-  } else if (xp >= 5001 && xp <= 7000) {
+  } else if (heroi.xp >= 5001 && heroi.xp <= 7000) {
     nivel = "Ouro";
-  } else if (xp >= 7001 && xp <= 8000) {
+  } else if (heroi.xp >= 7001 && heroi.xp <= 8000) {
     nivel = "Platina";
-  } else if (xp >= 8001 && xp <= 9000) {
+  } else if (heroi.xp >= 8001 && heroi.xp <= 9000) {
     nivel = "Ascendente";
-  } else if (xp >= 9001 && xp <= 10000) {
+  } else if (heroi.xp >= 9001 && heroi.xp <= 10000) {
     nivel = "Imortal";
-  } else if (xp >= 10001) {
+  } else if (heroi.xp >= 10001) {
     nivel = "Radiante";
   }
 
   // Mensagem final
-  console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+  console.log("O Herói de nome " + heroi.nome + " está no nível de " + nivel);
 }
